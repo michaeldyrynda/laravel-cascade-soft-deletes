fb2538866856ca895f681c671246b503fcd0f4f4
Michael Dyrynda <michael@dyrynda.com.au>
2016-09-08 23:19:45 +0930

    Refactor processing of cascaded soft deletes
    Resolves #13
    
    I've broken the functionality out into separate methods and hopefully
    tidied this package up a little in the process.
    
    I've tweaked the behaviour within the (old) static deleting method in
    order to handle many-to-many relationships, where a pivot table is
    present.
    
    Whilst this works and the failing test supplied now passes, it should be
    noted that this can actually be quite database heavy, as we iterate
    through each relationship and delete its records recursively. More
    records, more delete queries.
    
    I'm open to anybody having a better suggestion to handling this. I've
    spent quite a while on it and come to the conclusion that there's a
    really good reason this stuff is implemented at the database level. If
    you're getting performance issues, it might be time to move away from
    soft deletes for this use-case. For smaller datasets, it ought to be
    "ok".

--- src/CascadeSoftDeletes.php
+++ src/CascadeSoftDeletes.php
@@ -19,30 +19,56 @@ trait CascadeSoftDeletes
     protected static function bootCascadeSoftDeletes()
     {
         static::deleting(function ($model) {
-            if (! $model->implementsSoftDeletes()) {
-                throw CascadeSoftDeleteException::softDeleteNotImplemented(get_called_class());
-            }
+            $model->validateCascadingSoftDelete();
 
-            if ($invalidCascadingRelationships = $model->hasInvalidCascadingRelationships()) {
-                throw CascadeSoftDeleteException::invalidRelationships($invalidCascadingRelationships);
-            }
-
-            $delete = $model->forceDeleting ? 'forceDelete' : 'delete';
-
-            foreach ($model->getActiveCascadingDeletes() as $relationship) {
-                if ($model->{$relationship} instanceof Model) {
-                    $model->{$relationship}->{$delete}();
-                } else {
-                    if ($model->{$relationship}() instanceof BelongsToMany) {
-                        $model->{$relationship}()->detach();
-                    } else {
-                        foreach ($model->{$relationship} as $child) {
-                            $child->{$delete}();
-                        }
-                    }
-                }
-            }
+            $model->runCascadingDeletes();
         });
+    }
+
+
+    /**
+     * Validate that the calling model is correctly setup for cascading soft deletes.
+     *
+     * @throws \Iatstuti\Database\Support\CascadeSoftDeleteException
+     */
+    protected function validateCascadingSoftDelete()
+    {
+        if (! $this->implementsSoftDeletes()) {
+            throw CascadeSoftDeleteException::softDeleteNotImplemented(get_called_class());
+        }
+
+        if ($invalidCascadingRelationships = $this->hasInvalidCascadingRelationships()) {
+            throw CascadeSoftDeleteException::invalidRelationships($invalidCascadingRelationships);
+        }
+    }
+
+
+    /**
+     * Run the cascading soft delete for this model.
+     *
+     * @return void
+     */
+    protected function runCascadingDeletes()
+    {
+        foreach ($this->getActiveCascadingDeletes() as $relationship) {
+            $this->cascadeSoftDeletes($relationship);
+        }
+    }
+
+
+    /**
+     * Cascade delete the given relationship on the given mode.
+     *
+     * @param  string  $relationship
+     * @return return
+     */
+    protected function cascadeSoftDeletes($relationship)
+    {
+        $delete = $this->forceDeleting ? 'forceDelete' : 'delete';
+
+        foreach ($this->{$relationship}()->get() as $model) {
+            $model->pivot ? $model->pivot->{$delete}() : $model->{$delete}();
+        }
     }
 
 
