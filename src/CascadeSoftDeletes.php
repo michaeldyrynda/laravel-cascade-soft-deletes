5be6d010ccb847c87f29009d98f958fe206aec41
unknown <haithamahmad16995@gmail.com>
2024-08-13 12:26:23 +0400

    adding auto restore cascaded relations

--- src/CascadeSoftDeletes.php
+++ src/CascadeSoftDeletes.php
@@ -22,6 +22,13 @@ trait CascadeSoftDeletes
 
             $model->runCascadingDeletes();
         });
+
+
+        static::restoring(function ($model) {
+            $model->validateCascadingSoftDelete();
+
+            $model->runRestoreCascadingDeletes();
+        });
     }
 
 
@@ -55,6 +62,20 @@ trait CascadeSoftDeletes
     }
 
 
+
+    /**
+     * Run the restore cascading deletes process.
+     *
+     * @return void
+     */
+    protected function runRestoreCascadingDeletes()
+    {
+        foreach ($this->getDeletedCascadingDeletes() as $relationship) {
+            $this->cascadeRestores($relationship);
+        }
+    }
+
+
     /**
      * Cascade delete the given relationship on the given mode.
      *
@@ -67,6 +88,21 @@ trait CascadeSoftDeletes
 
         foreach ($this->{$relationship}()->get() as $model) {
             isset($model->pivot) ? $model->pivot->{$delete}() : $model->{$delete}();
+        }
+    }
+
+
+
+    /**
+     * Cascade restores the related models for a given relationship.
+     *
+     * @param string $relationship The name of the relationship.
+     * @return void
+     */
+    protected function cascadeRestores($relationship)
+    {
+        foreach ($this->{$relationship}()->onlyTrashed()->get() as $model) {
+            $model->restore();
         }
     }
 
@@ -119,5 +155,16 @@ trait CascadeSoftDeletes
         return array_filter($this->getCascadingDeletes(), function ($relationship) {
             return $this->{$relationship}()->exists();
         });
+    }
+
+
+    /**
+     * Retrieves the deleted cascading deletes.
+     *
+     * @return mixed
+     */
+    protected function getDeletedCascadingDeletes()
+    {
+        return $this->getCascadingDeletes();
     }
 }
